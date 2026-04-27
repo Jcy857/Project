3b7be2794439f03b95e0a424817e3125ec16ac86
Jcy857 <chengkanpang@gmail.com>
2026-04-27 23:46:53 +0800

    Final

--- inference.py
+++ inference.py
@@ -1,66 +1,174 @@
 import torch
-import numpy as np
 import pandas as pd
 
 # Import the model class from train.py
-from train import StochasticControlModel, set_seed
+from train import StochasticControlModel
 
 # ========================= CONFIG =========================
-T = 10
+T = 20
 TARGET_SHARES = 1000.0
-IMPACT_FACTOR = 0.00001
-state_dim = 20
-control_dim = state_dim
-hidden_dim = 100
+IMPACT_FACTOR = 0.0001
+STATE_DIM = 10
+CONTROL_DIM = 10
+HIDDEN_DIM = 100
+IMPACT_DIM = 3
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-if __name__ == "__main__":
-    set_seed(1234)
-
-    model = StochasticControlModel(T=T, 
-                               state_dim=state_dim, 
-                               control_dim=control_dim, 
-                               hidden_dim=hidden_dim).to(DEVICE)
-    model.load_state_dict(torch.load("sde_model.pt", map_location=DEVICE))
+def run_model(model_path, prices, A_mat, B_mat, C_mat):
+    model = StochasticControlModel(
+        T=T, 
+        state_dim=STATE_DIM, 
+        control_dim=CONTROL_DIM, 
+        hidden_dim=HIDDEN_DIM,
+        impact_dim=IMPACT_DIM,
+        impact_factor=IMPACT_FACTOR
+    ).to(DEVICE)
+    
+    model.load_state_dict(torch.load(model_path, map_location=DEVICE))
     model.eval()
-    print("Model loaded successfully for evaluation.\n")
+    print(f"--- Model loaded from {model_path} ---")
 
-    # Load test data
+    
+    s_t = torch.full((1, STATE_DIM), TARGET_SHARES, device=DEVICE)
+    x_t = torch.zeros(1, IMPACT_DIM, device=DEVICE).unsqueeze(-1)
+
+    total_model_cost: torch.Tensor = torch.zeros(1, device=DEVICE)
+
+    print(f"\n{'Step':<5} | {'Action (Avg)':<12} | {'Remaining (Avg)':<15} | {'Step Cost':<10}")
+    print("-" * 60)
+
+    with torch.no_grad():
+        prices = prices[:T].unsqueeze(0).to(DEVICE)
+        _, actions = model.compute_loss(s_t, prices, return_actions=True)
+        if actions is not None:
+            for t in range(T):
+                a_t = actions.T[t].unsqueeze(-1)
+                p_t = prices[:, t].to(torch.float32).unsqueeze(-1)
+                P_t_tilde = torch.diag_embed(p_t.squeeze(-1))
+
+                impact_price = P_t_tilde @ (A_mat @ P_t_tilde @ a_t + B_mat @ x_t)
+                execution_price = p_t + impact_price
+
+                step_cost = torch.sum(execution_price * a_t, dim=1).flatten()
+                total_model_cost += step_cost
+
+                s_t = s_t - a_t
+                x_t = C_mat @ x_t + torch.randn(IMPACT_DIM, 1, device=DEVICE)
+
+                print(f"{t:<5} | {a_t.mean().item():<12.2f} | {s_t.mean().item():<15.2f} | {step_cost.mean().item():,.2f}")
+    print("-" * 60)
+    return total_model_cost.mean()
+
+def run_instant(A_mat, B_mat, C_mat):
+    x_t = torch.zeros(1, IMPACT_DIM, 1, device=DEVICE)
+    instant_qty = TARGET_SHARES
+    p_t = prices[0].view(STATE_DIM, 1)
+    a_instant = torch.full((STATE_DIM, 1), instant_qty, device=DEVICE)
+    P_t_tilde = torch.diag_embed(p_t.squeeze(-1))
+    exec_p = p_t + (P_t_tilde @ (A_mat @ P_t_tilde @ a_instant + B_mat @ x_t))
+    total_instant_cost = torch.sum(exec_p * a_instant)
+    return total_instant_cost
+
+def run_equal_split(A_mat, B_mat, C_mat):
+    x_t = torch.zeros(1, IMPACT_DIM, 1, device=DEVICE)
+    equal_qty = TARGET_SHARES / T
+    total_equal_cost = 0.0
+    for t in range(T):
+        p_t = prices[t].view(STATE_DIM, 1)
+        a_eq = torch.full((STATE_DIM, 1), equal_qty, device=DEVICE)
+        P_t_tilde = torch.diag_embed(p_t.squeeze(-1))
+        exec_p = p_t + (P_t_tilde @ (A_mat @ P_t_tilde @ a_eq + B_mat @ x_t))
+        total_equal_cost += torch.sum(exec_p * a_eq).item()
+    return total_equal_cost
+
+def solve_analytical_recursion(T, A_mat, B_mat, C_mat):
+    """
+    Implements the recursive equations for Ak and Bk from the provided image.
+    k represents steps remaining.
+    """
+    A_list:torch.Tensor = [None] * (T + 1)
+    B_list:torch.Tensor = [None] * (T + 1)
+
+    # Base cases: A0 = A, B0 = B'
+    A_list[0] = A_mat
+    B_list[0] = B_mat.t()
+
+    for k in range(1, T + 1):
+        # Ak = A - 1/4 * A * inv(Ak-1) * A'
+        inv_prev_A = torch.linalg.inv(A_list[k-1])
+        A_list[k] = A_mat - 0.25 * (A_mat @ inv_prev_A @ A_mat.t())
+
+        # Bk = 1/2 * C' * Bk-1 * inv(Ak-1') * A' + B'
+        B_list[k] = (0.5 * (C_mat.t() @ B_list[k-1]) @ torch.linalg.inv(A_list[k-1].t()) @ A_mat.t()) + B_mat.t()
+
+    return A_list, B_list
+
+def run_analytic(A_mat, B_mat, C_mat):
+    A_list, B_list = solve_analytical_recursion(T, A_mat, B_mat, C_mat)
+    s_t = torch.full((STATE_DIM, 1), TARGET_SHARES, device=DEVICE)
+    x_t = torch.zeros(IMPACT_DIM, 1, device=DEVICE)
+    total_analytical_cost = 0.0
+    
+    print(f"\n{'Step':<5} | {'Action (Avg)':<12} | {'Remaining (Avg)':<15} | {'Step Cost':<10}")
+    print("-" * 60)
+
+    for t in range(T):
+        k = T - t - 1
+        idx = k - 1 
+        inv_Ak = torch.linalg.inv(A_list[idx])
+        
+        # Eq (10): s*_T-k = (I - 1/2 * inv(Ak-1) * A') * w_T-k + 1/2 * inv(Ak-1) * Bk-1' * C * x_T-k
+        I_mat = torch.eye(STATE_DIM, device=DEVICE)
+        a_t = (I_mat - 0.5 * (inv_Ak @ A_mat.t())) @ s_t + 0.5 * (inv_Ak @ B_list[idx].t() @ C_mat @ x_t)
+        # Ensure feasibility (Hard constraint for final step)
+        if t == T - 1:
+            a_t = s_t
+        else:
+            a_t = torch.minimum(a_t, s_t)
+
+        # Cost Calculation (Market Impact)
+        p_t = prices[t].view(STATE_DIM, 1)
+        P_t_tilde = torch.diag_embed(p_t.squeeze(-1))
+        
+        # Impact model: delta = P_t_tilde * (A * P_t_tilde * a + B * x)
+        impact_price = P_t_tilde @ (A_mat @ P_t_tilde @ a_t + B_mat @ x_t)
+        execution_price = p_t + impact_price
+        
+        step_cost = torch.sum(execution_price * a_t).item()
+        total_analytical_cost += step_cost
+        
+        s_t = s_t - a_t
+        x_t = C_mat @ x_t + torch.randn(IMPACT_DIM, 1, device=DEVICE)
+        print(f"{t:<5} | {a_t.mean().item():<12.2f} | {s_t.mean().item():<15.2f} | {step_cost:,.2f}")
+
+
+    print("-" * 60)
+    return total_analytical_cost
+
+if __name__ == "__main__":
+    # Load and Prepare Test Data
     df = pd.read_csv("test.csv")
     if "Date" in df.columns:
-        df["Date"] = pd.to_datetime(df["Date"])
         df = df.set_index("Date")
     df = df.dropna(axis=1)
-    price_data = torch.tensor(df.values[:, :state_dim], dtype=torch.float32).to(DEVICE)
-    s0_single = torch.full((control_dim,), TARGET_SHARES, device=DEVICE)
-    optimal_a = model.get_optimal_controls(s0_single)
-    remaining = s0_single.clone()
-    total_execution_cost = torch.zeros(control_dim, device=DEVICE)
+    prices = torch.tensor(df.values[:, :STATE_DIM], dtype=torch.float32, device=DEVICE)
 
-    for t, a_t in enumerate(optimal_a):
-        # Update remaining shares
-        a_t  = torch.as_tensor(a_t, device=DEVICE).flatten()
-        remaining -= a_t
-        p_t = price_data[t]
+    # Pre-define matrices (Matching training logic)
+    A_mat = torch.eye(STATE_DIM, device=DEVICE) * IMPACT_FACTOR # Assume no cross stock impact for simplicity
+    B_mat = torch.zeros(STATE_DIM, IMPACT_DIM, device=DEVICE)   # Set to 0 for simplicity
+    C_mat = torch.zeros(IMPACT_DIM, IMPACT_DIM, device=DEVICE)  # Set to 0 for simplicity
+    
+    # Final Summary Table
+    print("\n" + "="*60)
+    total_model_cost = run_model("sde_model.pt", prices, A_mat, B_mat, C_mat)
+    total_equal_cost = run_equal_split(A_mat, B_mat, C_mat)
+    total_instant_cost = run_instant(A_mat, B_mat, C_mat)
+    total_analytical_cost = run_analytic(A_mat, B_mat, C_mat)
 
-        # Linear impact cost for this step
-        step_cost = p_t * a_t * (1 + IMPACT_FACTOR * a_t)
-        total_execution_cost += step_cost
-
-    total_execution_cost = np.sum(total_execution_cost.cpu().numpy())
-    print(f"Approximate total execution cost   : {total_execution_cost:,.2f}")
-
-    # Instant purchase baseline
-    instant_cost = np.sum(price_data[0].cpu().numpy() * TARGET_SHARES * (1 + IMPACT_FACTOR * TARGET_SHARES))
-    print(f"Instant purchase cost (all at t=0) : {instant_cost:,.2f}")
-
-    # Naive equal split baseline
-    equal_split_cost = torch.zeros(control_dim, device=DEVICE)
-    for t in range(T):
-        p_t = price_data[t]
-        a_t = TARGET_SHARES / T
-        step_cost = p_t * a_t * (1 + IMPACT_FACTOR * a_t)
-        equal_split_cost += step_cost
-    equal_split_cost = np.sum(equal_split_cost.cpu().numpy())
-    print(f"Equal split cost (naive)           : {equal_split_cost:,.2f}")
-
+    print(f"{'STRATEGY':<30} | {'TOTAL COST':<20}")
+    print("-" * 60)
+    print(f"{'Stochastic Model (AI)':<30} | {total_model_cost:,.2f}")
+    print(f"{'Analytical DP Cost :':<30} | {total_analytical_cost:,.2f}")
+    print(f"{'Equal Split (Naive)':<30} | {total_equal_cost:,.2f}")
+    print(f"{'Instant Purchase (t=0)':<30} | {total_instant_cost:,.2f}")
+    print("="*60)
--- train.py
+++ train.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import random
-import numpy as np
 import pandas as pd
 import os
+from typing import Tuple
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
@@ -15,7 +14,7 @@ class ControlSubNet(nn.Module):
     - 1 input layer (state_dim) + 2 hidden layers (hidden_dim neurons each) + 1 output layer (control_dim).
     - ReLU activation on hidden layers only (no activation on final layer by default).
     - Batch normalization after each linear transformation and before activation (for hidden layers).
-    - Weights initialized from normal distribution (mean=0, std=0.1).
+    - Weights initialized from normal distribution (mean=0, std=1).
     """
     def __init__(self, 
                  state_dim: int, 
@@ -26,14 +25,14 @@ class ControlSubNet(nn.Module):
 
         # Linear layers
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.bn1 = nn.BatchNorm1d(hidden_dim)
+        self.bn1 = nn.LayerNorm(hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.bn2 = nn.BatchNorm1d(hidden_dim)
+        self.bn2 = nn.LayerNorm(hidden_dim) # Oringial paper use batchnorm1d
         self.fc_out = nn.Linear(hidden_dim, control_dim)
 
         # Normal initialization for all weights
         for layer in [self.fc1, self.fc2, self.fc_out]:
-            nn.init.normal_(layer.weight, mean=0, std=0.1)
+            nn.init.normal_(layer.weight, mean=0, std=1)
             if layer.bias is not None:
                 nn.init.constant_(layer.bias, 0)
 
@@ -54,9 +53,17 @@ class StochasticControlModel(nn.Module):
     def __init__(self, T: int, 
                  state_dim: int, 
                  control_dim: int, 
-                 hidden_dim: int):
+                 hidden_dim: int,
+                 impact_dim: int,
+                 impact_factor: float):
         super().__init__()
         self.T = T
+        self.state_dim = state_dim
+        self.control_dim = control_dim
+        self.hidden_dim = hidden_dim
+        self.impact_dim = impact_dim
+        self.impact_factor = impact_factor
+        
         self.policies = nn.ModuleList([
             ControlSubNet(state_dim, control_dim, hidden_dim)
             for _ in range(T)
@@ -64,7 +71,8 @@ class StochasticControlModel(nn.Module):
 
     def compute_loss(self,
                      s0: torch.Tensor,
-                     price_path: torch.Tensor) -> torch.Tensor:
+                     prices: torch.Tensor, 
+                     return_actions=False) -> Tuple[torch.Tensor, torch.Tensor|None]:
         """
         Forward pass that unrolls the trajectory and computes the expected total cost (Monte-Carlo approximation).
         
@@ -75,107 +83,108 @@ class StochasticControlModel(nn.Module):
         Returns:
             Scalar loss = E[C_T] approximated by batch mean.
         """
+        
+        state_dim = self.state_dim
+        impact_dim = self.impact_dim
         batch_size = s0.shape[0]
         device = s0.device
-        s = s0.clone()
-        total_cost = torch.zeros(batch_size, device=device)
+        impact_factor = self.impact_factor
+        actions_history = []
 
+        # Monte-Carlo estimate of total cost for 64 trajectories in the batch
+        s_t = s0.clone().unsqueeze(-1).to(device)  # (batch_size, state_dim, 1)
+        total_cost: torch.Tensor = torch.zeros(batch_size, device=device)
+        A_mat = torch.eye(state_dim, device=device) * impact_factor # Assume no cross stock impact for simplicity
+        B_mat = torch.zeros(state_dim, impact_dim, device=device)   # Set to 0 for simplicity
+        C_mat = torch.zeros(impact_dim, impact_dim, device=device)  # Set to 0 for simplicity
+
+        # Potential influence of market conditions
+        x_t = torch.zeros(batch_size, impact_dim, device=device).unsqueeze(-1)
         for t in range(self.T):
-            # Control from sub-network at time t
-            a = self.policies[t](s)  # (batch_size, control_dim)
-            a = torch.clamp(a, min=0.0)
-            a = torch.minimum(a, s)
-            
+            # Hard constraint
+            if t < self.T - 1:
+                a_t = self.policies[t](s_t.squeeze(-1)) 
+                a_t = torch.minimum(a_t, s_t.squeeze(-1)).unsqueeze(-1)
+            else:
+                a_t = s_t
 
-            # Linear percentage price impact model
-            p_t = price_path[:, t].to(torch.float32)
-            impact_price = p_t * (1.0 + IMPACT_FACTOR * a)
-            execution_cost = torch.sum(impact_price * a, dim=1)
+            if return_actions:
+                actions_history.append(a_t)
 
-            total_cost += execution_cost
+            # Market Dynamics
+            p_t = prices[:, t].to(torch.float32).to(device).unsqueeze(-1)
+            P_t_tilde = torch.diag_embed(p_t.squeeze(-1))
+
+
+            # Step Cost: Sum over the assets dimension
+            impact_price = P_t_tilde @ (A_mat @ P_t_tilde @ a_t + B_mat @ x_t)
+            execution_price = p_t + impact_price
+
+            step_cost = torch.sum(execution_price * a_t, dim=1).flatten()
+            total_cost += step_cost
 
             # Update remaining shares
-            s = torch.clamp(s - a, min=0.0)
-
-
-        # Terminal cost: Buy the remaining shares at the final price
-        p_T = price_path[:, -1]  # price at final time step
-        terminal_cost = torch.sum(p_T * s * (1.0 + IMPACT_FACTOR * s), dim=1)
-        total_cost += terminal_cost
-        # Return Monte-Carlo estimate of E[C_T]
-        return total_cost.mean()
+            s_t = s_t - a_t
+            x_t = C_mat @ x_t + torch.randn(batch_size, impact_dim, 1, device=device)
+            
+        if return_actions:
+            actions = torch.stack(actions_history)
+            actions = actions.squeeze(-1)
+            avg_actions = actions.mean(dim=1)
+            avg_trajectory = avg_actions.T
+            # Stack into (T, batch_size, state_dim)
+            return total_cost.mean(), avg_trajectory
     
-    def get_optimal_controls(self, s0: torch.Tensor) -> list[torch.Tensor]:
-        """
-        Returns the optimal control a_t for each time step t.
-        """
-        if s0.dim() == 1:
-            s0 = s0.unsqueeze(0)
+        # Return Monte-Carlo estimate of E[C_T]
+        return total_cost.mean(), None
+    
         
-        s = s0.clone().to(torch.float32)
-        controls = []
-        
-        with torch.no_grad():
-            for t in range(self.T):
-                a = self.policies[t](s)
-                a = torch.clamp(a, min=0.0)
-                a = torch.minimum(a, s)
-                controls.append(a)
-                s = torch.clamp(s - a, min=0.0)
-        
-        return controls
-
 # Training loop
-def train_portfolio(price_data: torch.Tensor,
+def train_portfolio(prices: torch.Tensor,
                     T: int, 
                     state_dim: int, 
                     control_dim: int,
                     hidden_dim: int, 
+                    impact_dim: int,
+                    impact_factor: float,
                     batch_size: int,
                     num_iterations: int, 
                     lr: float):
 
-
     model = StochasticControlModel(T=T, 
                                    state_dim=state_dim, 
                                    control_dim=control_dim,
-                                   hidden_dim=hidden_dim).to(DEVICE)
+                                   hidden_dim=hidden_dim,
+                                   impact_dim = impact_dim,
+                                   impact_factor=impact_factor).to(DEVICE)
 
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     print(f"Training started on {DEVICE}:".ljust(25)+ f" | T={T} | Iterations={num_iterations}")
 
     for iter_idx in range(num_iterations):
-        # Sample random starting time indices to avoid overfitting to the same trajectories
-        start_idx = np.random.randint(0, price_data.shape[0] - T, batch_size)
-
-        s0 = torch.full((batch_size, control_dim), TARGET_SHARES, device=DEVICE)
+        s0 = torch.full((batch_size, state_dim), TARGET_SHARES, device=DEVICE)
 
         # Get price path for this batch
-        batch_prices = torch.stack([price_data[i:i+T] for i in start_idx])
-        batch_prices = batch_prices.to(DEVICE)
+        import numpy as np
+        start_idx = np.random.randint(0, prices.shape[0] - T, batch_size)
+        batch_prices = torch.stack([prices[i:i+T] for i in start_idx]).to(DEVICE)
+        #batch_prices = prices[:T].unsqueeze(0).repeat(batch_size, 1, 1).to(DEVICE)
 
-        loss = model.compute_loss(s0, batch_prices)
+        loss, _ = model.compute_loss(s0, batch_prices) 
         optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
 
-        if iter_idx % 500 == 0:
-            print(f"Iter: {iter_idx:5d}".ljust(25) + f" | Avg Execution Cost: {loss.item():.2f}")
+        if iter_idx % 100 == 0 or iter_idx == num_iterations - 1:
+            print(f"Iter: {iter_idx:5d}".ljust(25) + f" | Avg Execution Cost: {loss.item():,.2f}")
+        
     print("Training completed!")
     return model
 
-def set_seed(seed=42):
-    random.seed(seed)                # Python random
-    np.random.seed(seed)             # NumPy
-    torch.manual_seed(seed)          # PyTorch CPU
-    torch.cuda.manual_seed(seed)     # PyTorch GPU
-
 if __name__ == "__main__":
-    set_seed(1234)
-    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
+    #torch.manual_seed(42)
     # Load data
     df = pd.read_csv("train.csv")
     if "Date" in df.columns:
@@ -184,32 +193,34 @@ if __name__ == "__main__":
     df = df.dropna(axis=1)
 
     # Hyper-parameters matching the paper
-    T = 10
+    T = 20
     TARGET_SHARES = 1000.0
-    IMPACT_FACTOR = 0.00001
-    state_dim = 20
-    control_dim = state_dim
-    hidden_dim = 100
-    batch_size = 64                
-    num_iterations = 15000
-    lr = 0.001
+    IMPACT_FACTOR = 0.0001
+    STATE_DIM = 10
+    CONTROL_DIM = 10
+    HIDDEN_DIM = 100 
+    IMPACT_DIM = 3
+    BATCH_SIZE = 64 
+    NUM_ITERATIONS = 1000 #15000
+    LR = 0.001
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
     # Convert to PyTorch tensor
-    price_data = torch.tensor(df.values[:, :state_dim], dtype=torch.float32).to(DEVICE)
-    print(f"Initial shares per stock : {TARGET_SHARES:,.0f}")
-    print(f"Number of stocks         : {control_dim}")
-
+    prices = torch.tensor(df.values[:, :STATE_DIM], dtype=torch.float32, device=DEVICE)
 
     # Train the model
     model = train_portfolio(
-        price_data=price_data,
+        prices=prices,
         T=T,
-        state_dim=state_dim,
-        control_dim=control_dim,
-        hidden_dim=hidden_dim,
-        batch_size=batch_size,
-        num_iterations=num_iterations,
-        lr=lr
+        state_dim=STATE_DIM,
+        control_dim=CONTROL_DIM,
+        hidden_dim=HIDDEN_DIM,
+        impact_dim=IMPACT_DIM,
+        impact_factor=IMPACT_FACTOR,
+        batch_size=BATCH_SIZE,
+        num_iterations=NUM_ITERATIONS,
+        lr=LR
     )    
 
     # Save model
